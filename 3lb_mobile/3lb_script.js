e48e2208a1efe61261bbe68e4f37a2faeed466d9
gqg228 <75837156+gqg228@users.noreply.github.com>
2021-04-12 14:35:58 +0300

    Add files via upload

--- 3lb_mobile/3lb_script.js
+++ 3lb_mobile/3lb_script.js
@@ -5,9 +5,6 @@ var vue = new Vue({
         numbers: [1,2,3,4,5,6,7,8,9,'.',0],
         operations: ['+','-','*','/'],
     },
-
-
-   
     methods:{
       input:function(char){
           this.result = this.result.toString()
@@ -26,3 +23,33 @@ var vue = new Vue({
       }
     }
 })
+const app = new Vue({
+
+  el: '#app2',
+  data: {
+    
+    tod: '',
+    todos: []
+  },
+  methods: {
+    addTodo() {
+      if(this.tod!=''){
+      this.todos.push({
+        title: this.tod,
+        done: false
+      });
+      this.tod = '';
+    }
+    },
+    removeTodo(todo) {
+      const dex = this.todos.indexOf(todo);
+      this.todos.splice(dex, 1);
+    },
+    allDone() {
+      this.todos.forEach(todo => {
+        todo.done = true;
+      });
+    }
+  }
+});
+
